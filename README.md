854add4419d8e76106f1f5a68fcf1324e98e6cd4
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2014-01-28 12:46:38 +0000

    seperated and adjusted badblock testing

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Puppet module for stresstesting hardware
 tests are scheduled and run using the crontab, install mail functionality for reporting the test results. A good combination is using the ssmtp module, the garex/puppet-module-ssmtp is a good example.
 the disk tests logs to /tmp, the logs are combined and send to the root user. 
 
+A max runtime for badblock testing is set for 2 hours, the hours are added to the starthour variable and together may not exceed 24 hours as it will result in incorrect cron time parameters.
+
 Parameters
 -------------
 All parameters have sensible defaults and can be overwritten by foreman or Hiera data files
--- manifests/init.pp
+++ manifests/init.pp
@@ -6,52 +6,82 @@ class stresstest (
   $cputest         = true,
   $memorytest      = true,
   $disktest        = true,
+  $badblocktest    = false,
+  $badblockruntime = 2,
   $disks           = ['sdb','sdc'],
-  $duration        = '60m',
+  $duration        = '120m',
   $starthour       = 12,
   $startminute     = 0,
+  $startmonth      = 1,
+  $startmonthday   = 1,
 ){
   if $cputest {
     $_cputest = " --cpu ${processorcount}"
   }
+
   if $memorytest {
     $amounttotest = $memorysize_mb * 0.80
     $_memorytest = " -m 1 --vm-bytes ${amounttotest}M"
   }
 
-  if $disktest {
-    package { 'smartmontools':
-      ensure      => present
-    }
-    $postcommand = " && /usr/local/sbin/disktest.sh"
-    file { '/usr/local/sbin/disktest.sh' :
-      content     => template("stresstest/disktest.sh.erb"),
-      mode        => 700,
-    }
-  }
-
   if ($_memorytest or $_cputest) {
-    package { 'stress':
-      ensure     => present
-    }
     $stressoptions = "${_cputest} ${_memorytest} --timeout ${duration}"
-    cron { 'stresstest' :
-      command    => template("stresstest/runstress.sh.erb"),
+    package { 'stress':
+      ensure      => present
+    } ->
+    file { '/usr/local/sbin/stresstest.sh' :
+      content     => template("stresstest/stresstest.sh.erb"),
+      mode        => 700,
+    } ->
+    cron { 'stresstest cpu' :
+      command    => "/usr/local/sbin/stresstest.sh",
       user       => 'root',
       minute     => $startminute,
       hour       => $starthour,
-      require    => Package['stress']
+      month      => $startmonth,
+      monthday   => $startmonthday,
     }
   }
-  if (!$_memorytest or !$_cputest) {
-    cron { 'stresstest' :
+
+  if ($disktest) {
+    package { 'smartmontools':
+      ensure     => present
+    } ->
+    file { '/usr/local/sbin/disktest.sh' :
+      content    => template("stresstest/disktest.sh.erb"),
+      mode       => 700,
+    } ->
+    cron { 'stresstest disk' :
       command    => "/usr/local/sbin/disktest.sh",
       user       => 'root',
       minute     => $startminute,
       hour       => $starthour,
-      require    => File['/usr/local/sbin/disktest.sh'],
+      month      => $startmonth,
+      monthday   => $startmonthday,
     }
   }
 
+  if ($badblocktest) {
+    file { '/usr/local/sbin/badblocktest.sh' :
+      content    => template("stresstest/badblocktest.sh.erb"),
+      mode       => 700,
+    } ->
+    cron { 'badblock test disk' :
+      command    => "/usr/local/sbin/badblocktest.sh",
+      user       => 'root',
+      minute     => $startminute,
+      hour       => $starthour,
+      month      => $startmonth,
+      monthday   => $startmonthday,
+    }
+    cron { 'badblock kill process' :
+      command    => "/usr/bin/killall /sbin/badblocks",
+      user       => 'root',
+      minute     => $startminute,
+      hour       => $starthour + $badblockruntime,
+      month      => $startmonth,
+      monthday   => $startmonthday,
+    }
+  }
 
 }
