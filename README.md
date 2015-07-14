ec919f20cc9efbb64967ffae2f3a1c9267815d1d
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2015-07-14 15:18:54 +0200

    updated stresstest, removed cron functionality

--- README.md
+++ README.md
@@ -2,14 +2,11 @@ puppet-stresstest
 ===================
 
 Puppet module for stresstesting hardware
-tests are scheduled and run using the crontab, install mail functionality for reporting the test results. A good combination is using the ssmtp module, the garex/puppet-module-ssmtp is a good example.
-the disk tests logs to /tmp, the logs are combined and send to the root user. 
-
-A max runtime for badblock testing is set for 2 hours, the hours are added to the starthour variable and together may not exceed 24 hours as it will result in incorrect cron time parameters.
+test can be started using /usr/local/sbin/stresstest.sh and logs to /tmp/stresstest.log
 
 Parameters
 -------------
-All parameters have sensible defaults and can be overwritten by foreman or Hiera data files
+All parameters have sensible defaults and can be overwritten in init.pp or Hiera data files
 
 Classes
 -------------
@@ -29,6 +26,7 @@ This module has been built on and tested against Puppet 3.4.2 and higher.
 
 The module has been tested on:
 ubuntu 12.04LTS
+ubuntu 14.04LTS
 
 Authors
 -------------
--- manifests/init.pp
+++ manifests/init.pp
@@ -7,94 +7,36 @@ class stresstest (
   $memorytest      = true,
   $disktest        = true,
   $badblocktest    = false,
-  $badblockruntime = 2,
   $disks           = ['sdb','sdc'],
-  $duration        = '120m',
-  $starthour       = 12,
-  $startminute     = 0,
-  $startmonth      = 1,
-  $startmonthday   = 1,
+  $duration        = '360m',
 ){
   if $cputest {
-    $_cputest = " --cpu ${processorcount}"
+    $_cputest = " --cpu ${stresstest::processorcount}"
   }
 
   if $memorytest {
-    $amounttotest = $memorysize_mb * 0.80
-    $_memorytest = " -m 1 --vm-bytes ${amounttotest}M"
+    $amounttotest = $::memorysize_mb * 0.80
+    $_memorytest  = " -m 1 --vm-bytes ${stresstest::amounttotest}M"
   }
 
   if ($_memorytest or $_cputest) {
     $stressoptions = "${_cputest} ${_memorytest} --timeout ${duration}"
     package { 'stress':
-      ensure      => present
-    } ->
-    file { '/usr/local/sbin/stresstest.sh' :
-      content     => template("stresstest/stresstest.sh.erb"),
-      mode        => 700,
-    } ->
-    cron { 'stresstest cpu' :
-      command    => "/usr/local/sbin/stresstest.sh",
-      user       => 'root',
-      minute     => $startminute,
-      hour       => $starthour,
-      month      => $startmonth,
-      monthday   => $startmonthday,
+      ensure    => present
     }
   }
 
   if ($disktest) {
     package { 'smartmontools':
-      ensure     => present
-    } ->
-    file { '/usr/local/sbin/disktest.sh' :
-      content    => template("stresstest/disktest.sh.erb"),
-      mode       => 700,
-    } ->
-    cron { 'stresstest disk' :
-      command    => "/usr/local/sbin/disktest.sh",
-      user       => 'root',
-      minute     => $startminute,
-      hour       => $starthour,
-      month      => $startmonth,
-      monthday   => $startmonthday,
+      ensure    => present
     }
   }
 
-
-  if $badblocktest {
-    file { '/usr/local/sbin/badblocktest.sh' :
-      content    => template("stresstest/badblocktest.sh.erb"),
-      mode       => 700,
-    } ->
-    cron { 'badblock test disk' :
-      command    => "/usr/local/sbin/badblocktest.sh",
-      user       => 'root',
-      minute     => $startminute,
-      hour       => $starthour,
-      month      => $startmonth,
-      monthday   => $startmonthday,
-    }
+  file { '/usr/local/sbin/stresstest.sh' :
+    content     => template('stresstest/stresstest.sh.erb'),
+    mode        => '0700',
   }
 
-  if $badblockruntime > 0 {
-    $killat = $starthour + $badblockruntime
-    if ($killat > 24) {
-      $killat = $killat - 24
-      $killatday = $startmonthday+1
-    } else {
-      $killatday = $startmonthday
-    }
-    if ($killat > 24) { 
-      $killat = 24
-    }
-    cron { 'badblock kill process' :
-      command    => "/usr/bin/killall /sbin/badblocks",
-      user       => 'root',
-      minute     => $startminute,
-      hour       => $starthour + $badblockruntime,
-      month      => $startmonth,
-      monthday   => $killatday,
-    }
-  }
+  notify{'Start /usr/local/sbin/stresstest.sh to run the stress test':}
+
 }
